1b0bfacb68c8b22b1acf78aa274da17e05b95ac8
Jelmer Cormont <jelmer@cormont.work>
2022-04-03 23:02:25 +0800

    Port forward to bastion not necessary with jumphost

--- files/.scripts/bastion.sh
+++ files/.scripts/bastion.sh
@@ -5,8 +5,6 @@ do
   ssh -v -N \
     -o ExitOnForwardFailure=yes \
     -R 11922:localhost:22 \
-    -R 11980:localhost:8080 \
-    -R 11988:localhost:8088 \
     bastion
   echo Reconnecting...
   sleep 20
