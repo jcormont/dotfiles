be72633436f23bec03ea6c8cdb2d8e70b8343f96
Jelmer Cormont <jelmer@cormont.work>
2022-08-22 10:32:36 +0800

    Update pack script for Mac

--- files/.scripts/pack.sh
+++ files/.scripts/pack.sh
@@ -1,3 +1,8 @@
-zip --exclude '*/node_modules/*' --exclude '*/deploy/*' -r3qdgds 10m Repositories\ $(date +%Y%m%d).zip Repositories
+cd
+echo --- Packing repositories
+zip --exclude '*/node_modules/*' --exclude '*/deploy/*' -r3qdgds 10m Backup/Repositories\ $(date +%Y%m%d).zip Dev/Repositories
+echo --- Packing Documents folder
+zip -ur0qdgds 10m Backup/Documents.zip Documents
+echo --- Packing Backup folder
 zip -ur0qdgds 10m Backup.zip Backup
 
