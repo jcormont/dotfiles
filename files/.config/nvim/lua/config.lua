3bec8dd797c21315fd908bafdafeee42c96f62e6
Jelmer Cormont <jelmer@cormont.work>
2022-10-03 21:34:33 +0800

    Don't use relative numbers anymore

--- files/.config/nvim/lua/config.lua
+++ files/.config/nvim/lua/config.lua
@@ -22,7 +22,6 @@ vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.autoread = true
 vim.opt.number = true
-vim.opt.relativenumber = true;
 vim.opt.cursorline = true
 vim.opt.scrolloff = 8
 vim.opt.splitbelow = true
